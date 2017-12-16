5f4e36c7941b2114f14d06020949dfd8230df5b8
timmyyeh <timmy0936570830@gmail.com>
2017-12-15 20:20:17 -0800

    Login Activity

--- app/src/main/java/com/anonymous/anonymous/LoginMainActivity.java
+++ app/src/main/java/com/anonymous/anonymous/LoginMainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class LoginMainActivity extends AppCompatActivity {
