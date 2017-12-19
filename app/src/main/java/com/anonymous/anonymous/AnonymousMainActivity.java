2fd5d3ee9654faae775d36fa1e99c46d89e9bf5a
timmyyeh <timmy0936570830@gmail.com>
2017-12-18 23:51:46 -0800

    top tool bar and bottom tool bar

--- app/src/main/java/com/anonymous/anonymous/AnonymousMainActivity.java
+++ app/src/main/java/com/anonymous/anonymous/AnonymousMainActivity.java
@@ -1,38 +1,79 @@
 package com.anonymous.anonymous;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 
-public class AnonymousMainActivity extends AppCompatActivity implements View.OnClickListener{
+public class AnonymousMainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anonymous_main);
 
-        findViewById(R.id.chat_imgbtn).setOnClickListener(this);
-        findViewById(R.id.logout_btn).setOnClickListener(this);
+        //top toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.top_toolbar_main);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Anonymous");
+
+        //bottom nav
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    // toolbar's overflow menu item
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.item_logout:
+                FirebaseAuth.getInstance().signOut();
+                switchActivity(LoginMainActivity.class);
+                return true;
+            case R.id.item_2:
+                Toast.makeText(getApplicationContext(), "You click item 2", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //init overflow menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.top_menu, menu);
+        return true;
     }
 
     @Override
-    public void onClick(View view) {
-        Toast.makeText(getApplicationContext(), "Did", Toast.LENGTH_SHORT).show();
-        int id = view.getId();
-        if(id == R.id.logout_btn){
-            FirebaseAuth.getInstance().signOut();
-            Intent intent = new Intent(AnonymousMainActivity.this, LoginMainActivity.class);
-            startActivity(intent);
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.ic_chat:
+                Toast.makeText(getApplicationContext(), "chat Selected", Toast.LENGTH_LONG).show();
+                switchActivity(ChatMainActivity.class);
+                return true;
+            case R.id.ic_discuss:
+                Toast.makeText(getApplicationContext(), "discussion selected", Toast.LENGTH_SHORT).show();
+                switchActivity(DiscussionMainActivity.class);
+                return true;
+            case R.id.ic_news:
+                Toast.makeText(getApplicationContext(), "news selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        else if(id == R.id.chat_imgbtn){
-            Intent intent = new Intent(AnonymousMainActivity.this, ChatMainActivity.class);
-            startActivity(intent);
-        }
+    }
+
+    private void switchActivity(Class<?> ActivityClass) {
+        Intent intent = new Intent(AnonymousMainActivity.this, ActivityClass);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/anonymous/anonymous/ChatMainActivity.java
+++ app/src/main/java/com/anonymous/anonymous/ChatMainActivity.java
@@ -7,21 +7,14 @@ import android.view.View;
 import android.widget.Toast;
 
 
-public class ChatMainActivity extends AppCompatActivity implements View.OnClickListener{
+public class ChatMainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_main);
 
-        findViewById(R.id.chat_imgbtn).setOnClickListener(this);
+
     }
 
-    @Override
-    public void onClick(View view) {
-        int id = view.getId();
-        if(id == R.id.chat_imgbtn){
-            Toast.makeText(getApplicationContext(), "WOW", Toast.LENGTH_SHORT).show();
-        }
-    }
 }
 
