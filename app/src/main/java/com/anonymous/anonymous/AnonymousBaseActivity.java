981e4bc655c9af9ee7fa4986a50163034a8fad43
timmyyeh <timmy0936570830@gmail.com>
2017-12-19 20:22:03 -0800

    clean up the code

--- app/src/main/java/com/anonymous/anonymous/AnonymousBaseActivity.java
+++ app/src/main/java/com/anonymous/anonymous/AnonymousBaseActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -15,17 +13,6 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public abstract class AnonymousBaseActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
-    BottomNavigationView bottomNavigationView;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_anonymous_main);
-
-        //bottom nav
-        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_nav_view);
-        bottomNavigationView.setOnNavigationItemSelectedListener(this);
-
-    }
 
     // toolbar's overflow menu item
     @Override
