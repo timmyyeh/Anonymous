b4fd3fceaa7befec5c09f754e1298433652ca1d4
timmyyeh <timmy0936570830@gmail.com>
2017-12-20 22:28:41 -0800

    custom adapter for displaying messages

--- app/src/main/java/com/anonymous/anonymous/Chat/ChatMainActivity.java
+++ app/src/main/java/com/anonymous/anonymous/Chat/ChatMainActivity.java
@@ -4,30 +4,29 @@ package com.anonymous.anonymous.Chat;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.anonymous.anonymous.AnonymousBaseActivity;
+import com.anonymous.anonymous.Chat.Model.ChatMessage;
 import com.anonymous.anonymous.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 
 public class ChatMainActivity extends AnonymousBaseActivity {
 
-    private ChatPagerAdapter pagerAdapter;
-
-    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_main);
-
-        pagerAdapter = new ChatPagerAdapter(getSupportFragmentManager());
-
-        viewPager = (ViewPager) findViewById(R.id.container);
-        setupViewPager(viewPager);
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.top_toolbar_main);
@@ -42,6 +41,26 @@ public class ChatMainActivity extends AnonymousBaseActivity {
         menuItem.setChecked(true);
         menuItem.setEnabled(false);
 
+        //write the object to Database after clicked
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText) findViewById(R.id.input);
+
+                //Write message
+                String user = FirebaseAuth.getInstance().getUid();
+
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .push()
+                        .setValue(new ChatMessage(input.getText().toString(), user));
+                //End of writing
+
+                input.setText("");
+            }
+        });
+
+        displayMessage();
 
     }
 
@@ -60,10 +79,17 @@ public class ChatMainActivity extends AnonymousBaseActivity {
         return super.onNavigationItemSelected(item);
     }
 
-    private void setupViewPager(ViewPager viewPager) {
-        ChatPagerAdapter adapter = new ChatPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new ChatFragment());
-        viewPager.setAdapter(adapter);
+    private void displayMessage(){
+        ListView listOfMessage = (ListView) findViewById(R.id.list_of_messages);
+
+
+        ArrayList<ChatMessage> chatMessages = new ArrayList<>();
+        chatMessages.add(new ChatMessage("Test", "Anonymous"));
+        ChatAdapter chatAdapter = new ChatAdapter(this, chatMessages);
+
+
+        listOfMessage.setAdapter(chatAdapter);
+
     }
 
 
